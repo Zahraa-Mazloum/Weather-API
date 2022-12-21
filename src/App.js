f132336b176e6b7702811f3338ac40ef8951b316
zahraa mazloum <zahraamazloum2001@gmail.com>
2022-12-21 13:21:23 +0200

    Handling error

--- src/App.js
+++ src/App.js
@@ -16,8 +16,20 @@ class App extends Component {
   fetchData = (n) => {
     let fetchData=(`http://api.openweathermap.org/data/2.5/forecast?q=${n}&cnt=8&units=metric&appid=af6aeff85cd304cc460ed995818338e4`)
     fetch(fetchData)
-    .then(res => res.json())
-    .then(data => {console.log(data); this.setState({allDayW: data}); this.setState({isLoaded: true})})
+    .then(res => {return res.json();
+  })
+  .then(data => {
+    if(data.cod!='200'){
+      alert('PLease enter a valid name')
+
+    }
+    else{
+      {console.log(data); this.setState({allDayW: data}); this.setState({isLoaded: true})}
+
+    }
+  }
+  )
+    
   }
 
   render() {
